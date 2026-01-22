3da9de11059a6a54e514189ca74cc7e9ab83ee8e
Kévin Tourret <kevin@drosalys.fr>
2026-01-22 15:15:45 +0100

    yolo

--- src/test/java/hesias/selenium/practices/correction/pages/BasePage.java
+++ src/test/java/hesias/selenium/practices/correction/pages/BasePage.java
@@ -21,7 +21,7 @@ public class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
     protected BasePage goTo(String url) {
