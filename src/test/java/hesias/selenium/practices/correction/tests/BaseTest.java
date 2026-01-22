fdfca9094228467eb947f7bfc37581bc3fcc5ff3
Kévin Tourret <kevin@drosalys.fr>
2026-01-22 12:18:00 +0100

    add ci to test practice

--- src/test/java/hesias/selenium/practices/correction/tests/BaseTest.java
+++ src/test/java/hesias/selenium/practices/correction/tests/BaseTest.java
@@ -4,7 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
