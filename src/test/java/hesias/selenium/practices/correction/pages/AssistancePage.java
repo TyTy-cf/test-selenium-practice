4d26a6ed4a57854a4b49a76325133652aa4e016b
Kévin Tourret <kevin@drosalys.fr>
2026-02-06 13:47:36 +0100

    fix select language

--- src/test/java/hesias/selenium/practices/correction/pages/AssistancePage.java
+++ src/test/java/hesias/selenium/practices/correction/pages/AssistancePage.java
@@ -2,19 +2,14 @@ package hesias.selenium.practices.correction.pages;
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import java.util.List;
-import java.util.Objects;
 
 public class AssistancePage extends BasePage {
 
     private final By titleAssistance = By.cssSelector("h1.h2");
     private final By languageSelect = By.cssSelector("div.dropdown.language-selector");
-    private final By languageItems = By.xpath("//a[@role='menuitem']");
+//    private final By languageItems = By.xpath("//a[@role='menuitem']");
+    private final String strLanguage = "//span//a[contains(@href, '%s')]";
 
     public AssistancePage(WebDriver driver) {
         super(driver);
@@ -26,17 +21,18 @@ public class AssistancePage extends BasePage {
 
     public AssistancePage selectLanguage(String locale) {
         waitClick(languageSelect).click();
-        try {
-            List<WebElement> webElementList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(languageItems));
-            for(WebElement webElement : webElementList) {
-                if (Objects.requireNonNull(webElement.getAttribute("href")).contains(locale)) {
-                    webElement.click();
-                }
-            }
-        } catch(StaleElementReferenceException ex)
-        {
-            return this;
-        }
+        waitClick(By.xpath(String.format(strLanguage, locale))).click();
+//        try {
+//            List<WebElement> webElementList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(languageItems));
+//            for(WebElement webElement : webElementList) {
+//                if (Objects.requireNonNull(webElement.getAttribute("href")).contains(locale)) {
+//                    webElement.click();
+//                }
+//            }
+//        } catch(StaleElementReferenceException ex)
+//        {
+//            return this;
+//        }
         return this;
     }
 
