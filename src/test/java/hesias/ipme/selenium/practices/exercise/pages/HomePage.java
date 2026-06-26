2887472774b395861081c07c06c4fc17c8e403b3
Victor Azevedo <hello@exemple.com>
2026-06-26 13:46:37 +0200

    Add ipme POM tests

---
+++ src/test/java/hesias/ipme/selenium/practices/exercise/pages/HomePage.java
@@ -0,0 +1,51 @@
+package hesias.ipme.selenium.practices.exercise.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+
+	private final By linkHome = By.cssSelector("a.menu__logo");
+	private final By shopDropdown = By.cssSelector("div[hook-test='menuStore']");
+	private final By helpDropdown = By.cssSelector("div[hook-test='menuSupport']");
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+
+	public HomePage open() {
+		goTo(ROOT_URL);
+		refuseCookies();
+		return this;
+	}
+
+	public HomePage changeLanguage(String locale, boolean refuseCookies) {
+		goTo(ROOT_URL.replace("fr", locale));
+		if (refuseCookies) {
+			refuseCookies();
+		}
+		return this;
+	}
+
+	public HomePage openShopDropdown() {
+		open();
+		moveToElement(shopDropdown);
+		return this;
+	}
+
+	public HomePage openNeedHelpDropdown() {
+		open();
+		moveToElement(helpDropdown);
+		return this;
+	}
+
+	public boolean isDisplayed() {
+		try {
+			waitUntil(linkHome);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+}
---
+++ src/test/java/hesias/ipme/selenium/practices/exercise/tests/HomeTest.java
@@ -0,0 +1,27 @@
+package hesias.ipme.selenium.practices.exercise.tests;
+
+import hesias.selenium.practices.correction.pages.HomePage;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Feature("Home page")
+@Severity(SeverityLevel.NORMAL)
+public class HomeTest extends BaseTest {
+
+	@Test
+	@Story("As an user, I want to access the home page")
+	@Severity(SeverityLevel.CRITICAL)
+	public void testHomePageIsDisplayed() {
+		boolean homeIsDisplayed = new HomePage(driver)
+				.open()
+				.isDisplayed();
+
+		assertTrue(homeIsDisplayed);
+	}
+
+}
